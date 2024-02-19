9c04faa7db520b6bd49af8f9d215dffc2f770c30
khoi <knguyen3531@gmail.com>
2024-02-19 21:42:54 +1100

    undo

--- migrations/20240219103601-create-posts.js
+++ migrations/20240219103601-create-posts.js
@@ -21,7 +21,7 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: 'Users',
           key: 'id'
